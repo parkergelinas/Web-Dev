43818ca67544fca69bc1103f5237a4cb4353a229
parkergelinas <87443891+parkergelinas@users.noreply.github.com>
2022-06-05 18:13:43 -0700

    Testing shit

---
+++ React Practice/my-test-app/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Button from './Button';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Test </h1>
+      <Button className="TestButton">
+        This is a test
+      </Button>
+      <h1> Test </h1>
+    </div>
+  );
+}
+
+export default App;
